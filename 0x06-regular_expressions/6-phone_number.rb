65f9a5bf363bb5e63a969a9e6cc17f7403a6576e
BrianSechelo <sechbrian123@gmail.com>
2024-02-27 12:38:59 -0800

    Task 6

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-# Pass the argument to the script
-p_number = ARGV[0]
-# Define the pattern for the regular expression
-pattern = /^\d{10}$/
-# check if p_number matches the pattern
-if p_number.match?(pattern)
-    puts p_number
-end
+puts ARGV[0].scan(/^\d{10,10}$/).join
