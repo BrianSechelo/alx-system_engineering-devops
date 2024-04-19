7a806490662a34e35b91bdaca6516c6e1fff7097
BrianSechelo <sechbrian123@gmail.com>
2024-04-19 15:13:17 -0700

    Update to task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Execute a commad
 exec { 'killmenow':
   command     => 'pkill killmenow',
-  path        => ['/bin', '/usr/bin'],
+  path        => ['/bin', '/usr/bin', '/usr/sbin'],
   refreshonly => true,
   notify      => File['/path/to/triggering_file'],
 }
