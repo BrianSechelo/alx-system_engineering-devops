bd013a450640d34661eb513a311fd8aecf742990
BrianSechelo <sechbrian123@gmail.com>
2024-06-04 11:37:00 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """This function queries the Redit API and returns the number of subscribers"""
-
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Retuns the total number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Custom"}
 
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
-        return 0
+        return -1
